725d06576ec7009e136dcdcecbf200fa31f7ea7a
Johnathan <jkrudy3@gmail.com>
2017-02-03 22:47:33 -0700

    added a background to the main menu

--- bin/Menu.java
+++ bin/Menu.java
@@ -1,12 +1,14 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Font;
+import java.awt.image.BufferedImage;
 
 public class Menu {
 
     private Game.STATE nextState;
     private Button start;
 
+    private BufferedImage background;
     
     public void mouseClick(int x, int y) {
 	if (start.contains(x,y)) {
@@ -19,7 +21,7 @@ public class Menu {
 	nextState = Game.STATE.Menu;
 	start = new Button(Game.SCRNWIDTH / 2 - 50, Game.SCRNHEIGHT / 2 - 25,
 			   "start", 100, 50);
-        
+        background = Game.images.getDir("background").get(1);
     }
 
     public Game.STATE getNextState() { return nextState; }
@@ -40,9 +42,12 @@ public class Menu {
 	g.drawRect(Game.WIDTH / 2 - 50, Game.HEIGHT / 2 - 32 + 64, 200, 64);
 	g.drawString("Play", Game.WIDTH / 2, Game.HEIGHT / 2 - 32 - 10);
 	*/
+	if (background != null) {
+	    g.drawImage(background,0,0,Game.SCRNWIDTH,Game.SCRNHEIGHT,null);
+	}
 
 	
-	g.drawRect(Game.WIDTH / 2 - 50, Game.HEIGHT / 2 - 32 - 64, 200, 64);
+	//g.drawRect(Game.WIDTH / 2 - 50, Game.HEIGHT / 2 - 32 - 64, 200, 64);
 	start.render(g);
     }
 }
